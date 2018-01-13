6a6734dd94bb53a46113d9f47e6341798d76880d
ztf <a13845678901@163.com>
2018-01-13 17:52:28 +0800

    注册etcd　创建任务

--- cmd/kapacitord/run/command.go
+++ cmd/kapacitord/run/command.go
@@ -15,6 +15,13 @@ import (
 	"github.com/masami10/kapacitor/server"
 	"github.com/masami10/kapacitor/services/logging"
 	"github.com/masami10/kapacitor/tick"
+	"github.com/coreos/etcd/clientv3"
+	"time"
+	"context"
+	"net/http"
+	"github.com/masami10/kapacitor/client/v1"
+	"bytes"
+	"strings"
 )
 
 const logo = `
@@ -116,6 +123,18 @@ func (cmd *Command) Run(args ...string) error {
 		return fmt.Errorf("write pid file: %s", err)
 	}
 
+	// registry to etcd
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints: strings.Split(config.EtcdServers, ","),
+		DialTimeout: 1 * time.Second,
+	})
+	if err != nil {
+		cmd.Logger.Fatal(err)
+	}
+	//
+	var keepAliveCh chan int
+	go cmd.registryToEtcd(config, cli, keepAliveCh)
+
 	// Create server from config and start it.
 	buildInfo := server.BuildInfo{Version: cmd.Version, Commit: cmd.Commit, Branch: cmd.Branch}
 	s, err := server.New(config, buildInfo, cmd.logService)
@@ -131,6 +150,9 @@ func (cmd *Command) Run(args ...string) error {
 
 	// Begin monitoring the server's error channel.
 	go cmd.monitorServerErrors()
+
+	//go cmd.monitorEtcdKeyChang()
+	go cmd.monitorTaskid(config, cli)
 
 	return nil
 }
@@ -160,6 +182,160 @@ func (cmd *Command) monitorServerErrors() {
 		}
 	}
 }
+
+func (cmd *Command) registryToEtcd(c *server.Config, cli *clientv3.Client, kch chan int) {
+	hostName := c.Hostname
+	key := "kapacitors/host_name/" + hostName
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	// grant
+	resp, err := cli.Grant(ctx, 5)
+	if err != nil {
+		fmt.Println(err)
+		cmd.Logger.Fatal(err)
+	}
+	cancel()
+
+	// put key
+	ctx2, cancel2 := context.WithTimeout(context.Background(), 1 * time.Second)
+	_, err = cli.Put(ctx2, key, hostName, clientv3.WithLease(resp.ID))
+	if err != nil {
+		fmt.Println(err)
+		cmd.Logger.Fatal(err)
+	}
+	cancel2()
+
+	ch, err := cli.KeepAlive(context.TODO(), resp.ID)
+	if err != nil {
+		cmd.Logger.Fatal(err)
+	}
+	go func() {
+		for kresp := range ch {
+			//kch <- 1
+			fmt.Println("TTL: ", kresp.TTL)
+		}
+	}()
+
+	<-kch
+}
+
+
+func (cmd *Command) monitorTaskid(c *server.Config, ecli *clientv3.Client){
+	// put delete
+
+	key := "tasks/id/"
+	rch := ecli.Watch(context.Background(), key, clientv3.WithPrefix())
+
+	for wresp := range rch {
+		for _, ev := range wresp.Events {
+			evType := ev.Type.String()
+			if evType == "PUT"{
+				//事务创建任务
+				taskId := string(ev.Kv.Value)
+
+				// 获取任务信息
+				taskConfigKey := "tasks/config/"+ taskId
+
+				fmt.Println("6666666666666666666666666666666666666666666666666666666666")
+				fmt.Println(taskConfigKey)
+
+				ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+				gresp, err := ecli.Get(ctx, taskConfigKey)
+				if err != nil {
+					log.Fatal(err)
+				}
+				cancel()
+
+				for _, ev := range gresp.Kvs {
+					if string(ev.Key) == taskConfigKey {
+						taskConfig := string(ev.Value)
+
+						var t client.Task
+						t, err = createTask(taskConfig)
+						if err != nil {
+							// 创建任务失败
+							cmd.Logger.Fatal(err)
+						}
+
+						keyIsCreated := "tasks/is_created/"+ taskId
+						_, err = ecli.Put(context.TODO(), keyIsCreated, "true")
+						if err != nil {
+							cmd.Logger.Fatal(err)
+						}
+						keyStatus := "tasks/status/"+ taskId
+						_, err = ecli.Put(context.TODO(), keyStatus, t.Status.String())
+
+						// 把任务数量加一
+						keyTaskNum := "kapacitors/task_num"
+						keyNodeTaskNum := "kapacitors/192.168.7.8/task_num"
+
+						taskNum := "1"
+						nodeTaskNum := "1"
+
+						resp, err := ecli.Get(context.TODO(), keyNodeTaskNum)
+						if err != nil {
+							cmd.Logger.Fatal(err)
+						}
+						for _, ev := range resp.Kvs {
+							if string(ev.Key) == keyNodeTaskNum {
+								num, _ := strconv.ParseInt(string(ev.Value), 10, 64)
+								nodeTaskNum = strconv.FormatInt(num+1, 10)
+							}
+						}
+
+						resp, err = ecli.Get(context.TODO(), keyTaskNum)
+						if err != nil {
+							cmd.Logger.Fatal(err)
+						}
+						for _, ev := range resp.Kvs {
+							if string(ev.Key) == keyTaskNum {
+								num, _ := strconv.ParseInt(string(ev.Value), 10, 64)
+								taskNum = strconv.FormatInt(num+1, 10)
+							}
+						}
+
+						_, err = ecli.Put(context.TODO(), keyNodeTaskNum, nodeTaskNum)
+						if err != nil {
+							cmd.Logger.Fatal(err)
+						}
+						_, err = ecli.Put(context.TODO(), keyTaskNum, taskNum)
+						if err != nil {
+							log.Fatal(err)
+						}
+
+					}
+				}
+
+			}else if evType == "DELETE" {
+
+			}
+		}
+	}
+}
+
+func createTask(task_config string) (client.Task, error) {
+	//client := &http.Client{}
+	url := "http://localhost:9092/kapacitor/v1/tasks"
+	fmt.Println(task_config)
+
+	var buf bytes.Buffer
+	buf.Write([]byte(task_config))
+	req, err := http.NewRequest("POST", url, &buf)
+	if err != nil {
+		//return client.Task{}, err
+	}
+	fmt.Println("88888888888888888888888888888888888888888888888888888888888888888")
+	req.Header.Set("Content-Type", "application/json")
+
+	t := client.Task{}
+
+	_, err = cli.Do(req, &t, http.StatusOK)
+
+	fmt.Println("777777777777777777777777777777777777777777777777777777777777777777")
+
+	return t, err
+}
+
 
 // ParseFlags parses the command line flags from args and returns an options set.
 func (cmd *Command) ParseFlags(args ...string) (Options, error) {
